f7c46c7b48973c1b2e638f5ed3b09640dcd2ceb1
Rdjadmrl <radjaadmiral00@gmail.com>
2024-01-23 10:26:28 +0700

    aaaa

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,8 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/rdjadmrl/public';
+    public string $baseURL = 'http://rdjdumas.rdj/';
+    // public string $baseURL = 'http://localhost/rdjadmrl/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
